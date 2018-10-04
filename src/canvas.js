e2efc1d1732c28cfb63f84c3c947828d9aa8a816
Gezim Haziri <gezim.h90@gmail.com>
2018-10-04 16:37:17 +0200

    clear unused code

--- src/canvas.js
+++ src/canvas.js
@@ -11,12 +11,11 @@ const mouse = {
     y: innerHeight / 2
 }
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
-
 // Event Listeners
 addEventListener('mousemove', event => {
     mouse.x = event.clientX
     mouse.y = event.clientY
+    drawMouse()
 })
 
 addEventListener('resize', () => {
@@ -26,46 +25,8 @@ addEventListener('resize', () => {
     init()
 })
 
-// Objects
-function Object(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-}
-
-Object.prototype.draw = function() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-}
-
-Object.prototype.update = function() {
-    this.draw()
-}
-
-// Implementation
-let objects
-function init() {
-    objects = []
-
-    for (let i = 0; i < 400; i++) {
-        // objects.push();
-    }
-}
-
 // Animation Loop
-function animate() {
-    requestAnimationFrame(animate)
+function drawMouse() {
     c.clearRect(0, 0, canvas.width, canvas.height)
-
     c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-    // objects.forEach(object => {
-    //  object.update();
-    // });
 }
-
-init()
-animate()
